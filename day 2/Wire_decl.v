9b8c0f42b407b3132f1113adfab9a7ee8ac5d52c
ASHU BABBAR <46401837+theelectronicguy@users.noreply.github.com>
2024-10-12 22:51:31 +0530

    Create Wire_decl.v

---
+++ day 2/Wire_decl.v
@@ -0,0 +1,15 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    wire temp1= a&b;
+    wire temp2= c&d;
+    assign out=temp1|temp2;
+    assign out_n=~out;
+            
+
+endmodule
